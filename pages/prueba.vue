1299447b4946f24244eb61b446d3c96f2eb7654b
rene018 <rene18agosto@gmail.com>
2023-04-10 12:39:17 -0500

    b

---
+++ pages/prueba.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <header class="flex h-[10vh] w-[90vw]">
+            <h1 class="text-center bg-gray-200">Poke Dex</h1>
+        </header>
+        <main>
+            <div>
+                <input type="text" placeholder="buscar pokemon">
+            </div>
+            <div>
+                <div>img<div></div>
+                    <h2>pica</h2>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+    
+<script>
+import axios from '@nuxtjs/axios'
+
+</script>
+    
+<style>
+body{
+    height: 100vh;
+}
+.title{
+    text-align: center;
+}
+</style>
