d558fc062ac3aaf6416f51d98d046381ae14d098
rene018 <rene18agosto@gmail.com>
2023-04-10 13:27:21 -0500

    co

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div>
+        <header class="flex justify-center m-4">
+          <div  class="flex justify-center items-center h-[10vh] w-[90vw] rounded-lg bg-gray-200">
+            <h1 class="text-6xl font-medium">Poke Dex</h1>
+          </div>
+
+        </header>
+        <main>
+          <div>
+            <div class="flex">
+                <input class="h-11" type="text" placeholder="buscar pokemon">
+            </div>
+            <div>
+                <div>img<div></div>
+                    <h2>pica</h2>
+                </div>
+            </div>
+          </div>
+
+        </main>
+    </div>
 </template>
     
 <script>
