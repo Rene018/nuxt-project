266118cb00fc4645400cab7c8daad6adbe4e8da7
rene018 <rene18agosto@gmail.com>
2023-04-11 13:44:59 -0500

    co

---
+++ components/target.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="flex flex-col bg-slate-200 p-3 rounded-xl" >
+        <div ><img class="w-72" :src="url" alt=""></div>
+        <h2 class="text-center font-bold text-3xl">{{ nombre }}</h2>
+    </div>
+</template>
+    
+<script>
+    export default{
+        name: 'target',
+        data(){
+            return{
+            }
+        },
+        props:{
+            nombre:{
+                type: String,
+                required: true
+            },
+            url:{
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
+    
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,57 @@
 <template>
     <div>
         <header class="flex justify-center m-4">
-          <div  class="flex justify-center items-center h-[10vh] w-[90vw] rounded-lg bg-gray-200">
-            <h1 class="text-6xl font-medium">Poke Dex</h1>
-          </div>
+            <div class="flex justify-center items-center h-[10vh] w-[90vw] rounded-lg bg-gray-200">
+                <h1 class="text-6xl font-medium">Poke Dex</h1>
+            </div>
 
         </header>
-        <main>
-          <div>
-            <div class="flex">
-                <input class="h-11" type="text" placeholder="buscar pokemon">
-            </div>
-            <div>
-                <div>img<div></div>
-                    <h2>pica</h2>
+        <main class="flex justify-center">
+            <div class="flex w-[98vw] flex-col">
+                <div class="flex">
+                    <input placeholder="buscar pokemon" class="h-11" type="text">
+                </div>
+                <div class="flex flex-wrap gap-10 ">
+                    <target v-for="i in response" :key="i" :nombre="i.name" :url="i.front_default"/>
                 </div>
             </div>
-          </div>
-
         </main>
     </div>
 </template>
     
 <script>
+export default {
+
+    name: 'IndexPage',
+    data() {
+        return {
+            response: [],
+            imgg: []
+        }
+    },
+    mounted() {
+        this.getInfo()
+        this.getImg()
+    },
+    methods: {
+        async getInfo(){
+            const {data} = await this.$axios.get('pokemon')
+            this.response= data.results
+
+        },
+        async getImg(){
+            for (let index = 0; index < this.response.length; index++) {
+            const {img} = await this.$axios.get(`pokemon/${this.response.name}`)
+            this.imgg= img.sprite
+                
+            }
+
+        },
 
 
+    }
+
+}
 </script>
     
-<style>
-body{
-    height: 100vh;
-}
-
-</style>
+<style></style>
