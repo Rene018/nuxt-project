a80b54181b26002c1bc169b0f1782cc35daa8a86
Rene018 <rene18agosto@gmail.com>
2023-04-12 09:34:30 -0500

    co

--- components/target.vue
+++ components/target.vue
@@ -1,6 +1,7 @@
 <template>
     <div class="flex flex-col bg-slate-200 p-3 rounded-xl" >
         <div ><img class="w-72" :src="url" alt=""></div>
+        <div>{{ url }}</div>
         <h2 class="text-center font-bold text-3xl">{{ nombre }}</h2>
     </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,57 +1,56 @@
 <template>
-    <div>
-        <header class="flex justify-center m-4">
-            <div class="flex justify-center items-center h-[10vh] w-[90vw] rounded-lg bg-gray-200">
-                <h1 class="text-6xl font-medium">Poke Dex</h1>
-            </div>
-
-        </header>
-        <main class="flex justify-center">
-            <div class="flex w-[98vw] flex-col">
-                <div class="flex">
-                    <input placeholder="buscar pokemon" class="h-11" type="text">
-                </div>
-                <div class="flex flex-wrap gap-10 ">
-                    <target v-for="i in response" :key="i" :nombre="i.name" :url="i.front_default"/>
-                </div>
-            </div>
-        </main>
-    </div>
+  <div>
+    <header class="flex justify-center m-4">
+      <div
+        class="flex justify-center items-center h-[10vh] w-[90vw] rounded-lg bg-gray-200"
+      >
+        <h1 class="text-6xl font-medium">Poke Dex</h1>
+      </div>
+    </header>
+    <main class="flex justify-center">
+      <div class="flex w-[98vw] flex-col">
+        <div class="flex">
+          <input placeholder="buscar pokemon" class="h-11" type="text" />
+        </div>
+        <div class="flex flex-wrap gap-10">
+          <target
+            v-for="i in response"
+            :key="i"
+            :nombre="i.name"
+            :url="i.id"
+          />
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
-    
+
 <script>
 export default {
-
-    name: 'IndexPage',
-    data() {
-        return {
-            response: [],
-            imgg: []
-        }
-    },
-    mounted() {
-        this.getInfo()
-        this.getImg()
-    },
-    methods: {
-        async getInfo(){
-            const {data} = await this.$axios.get('pokemon')
-            this.response= data.results
-
-        },
-        async getImg(){
-            for (let index = 0; index < this.response.length; index++) {
-            const {img} = await this.$axios.get(`pokemon/${this.response.name}`)
-            this.imgg= img.sprite
-                
-            }
-
-        },
-
-
+  name: "IndexPage",
+  data() {
+    return {
+      response: [],
+    };
+  },
+  mounted() {
+    this.getInfo();
+    for (let index = 0; index < 20; index++) {
+        this.getImg(index)
     }
 
-}
+  },
+  methods: {
+    async getInfo() {
+      const { data } = await this.$axios.get("pokemon");
+      this.response = data.results;
+    },
+    async getImg(i) {
+      const { data } = await this.$axios.get(`pokemon/${i}/`);
+       this.response[i].push(data.id) 
+    }
+  },
+};
 </script>
-    
+
 <style></style>
